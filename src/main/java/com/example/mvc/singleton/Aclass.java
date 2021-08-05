c6321ca4373643d52006714554768e35eafd5a2e
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-08-05 23:43:50 +0900

    Spring initial setting

---
+++ src/main/java/com/example/mvc/singleton/Aclass.java
@@ -0,0 +1,4 @@
+package com.example.mvc.singleton;
+
+public class Aclass {
+}
---
+++ src/main/java/com/example/mvc/singleton/SocketClient.java
@@ -0,0 +1,17 @@
+package com.example.mvc.singleton;
+
+
+public class SocketClient {
+    private static SocketClient socketClient=null;
+    private SocketClient() {}
+    public static SocketClient getInstance() {
+        if(socketClient == null) {
+            socketClient = new SocketClient();
+        }
+        return socketClient;
+    }
+    public void connect() {
+        System.out.println("connect");
+    }
+
+}
