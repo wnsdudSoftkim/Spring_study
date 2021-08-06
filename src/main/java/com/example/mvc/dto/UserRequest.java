75f1949e75aec99d26989a6e911b1c8c2dbc5c9a
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-08-06 15:44:03 +0900

    learn GetApiController  * string에서 string 을 더하는(+) 행위는 메모리 할당과 메모리 해제를 발생시키며 더하는 연산이 많아진다면   성능이 좋지않다. 그래서 나온 것이 StringBuilder이다  *Map.entrySet() entryset은 key value를 반환하기 위한 함수

---
+++ src/main/java/com/example/mvc/dto/UserRequest.java
@@ -0,0 +1,40 @@
+package com.example.mvc.dto;
+//현업에서 가장 자주 쓰이는 방법이다.
+public class UserRequest {
+    private String name;
+    private String email;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
